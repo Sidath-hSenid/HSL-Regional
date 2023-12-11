d96d9a53df050f2fac437a0473ed79cf3a0c7707
Sidath-hSenid <sidath.r@hsenidlanka.com>
2023-12-11 10:14:41 +0530

    Add local storage check

---
+++ regional_app/lib/screens/fail_screen.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:regional_app/utils/constants.dart';
+
+class FailScreen extends StatelessWidget {
+  const FailScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    FailureScreen screenConstants = FailureScreen();
+    var size = MediaQuery.of(context).size;
+    return Scaffold(
+      appBar: AppBar(
+          backgroundColor: screenConstants.appBarColor,
+          centerTitle: true,
+          title: Text(
+            screenConstants.appBarTitle,
+            style: TextStyle(
+              fontFamily: screenConstants.textFont,
+              fontSize: 24,
+              color: screenConstants.textColor,
+              height: 1,
+            ),
+          )),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: SizedBox(
+              child: Container(
+                height: size.height / 3,
+                width: (size.width * 2) / 3,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(24.0),
+                  color: screenConstants.appColor,
+                ),
+                child: Center(
+                  child: Text(
+                    screenConstants.sizedBoxText,
+                    style: TextStyle(
+                      fontFamily:screenConstants.textFont,
+                      fontSize: 18,
+                      color: screenConstants.textColor,
+                      height: 1,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
--- regional_app/lib/screens/home_screen.dart
+++ regional_app/lib/screens/home_screen.dart
@@ -1,5 +1,8 @@
 import 'package:firebase_remote_config/firebase_remote_config.dart';
 import 'package:flutter/material.dart';
+import 'package:regional_app/screens/fail_screen.dart';
+import 'package:regional_app/screens/success_screen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import '../utils/constants.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -11,7 +14,6 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   ApplicationConstants appConstants = ApplicationConstants();
-
   final FirebaseRemoteConfig _remoteConfig = FirebaseRemoteConfig.instance;
 
   void _initConfig() async {
@@ -49,27 +51,58 @@ class _HomeScreenState extends State<HomeScreen> {
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Center(
-            child: SizedBox(
-              child: Container(
-                height: size.height / 3,
-                width: (size.width * 2) / 3,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(24.0),
-                  color: appConstants.getAppColor(),
-                ),
-                child: Center(
-                  child: Text(
-                    appConstants.getSizedBoxText(),
-                    style: TextStyle(
-                      fontFamily: appConstants.getTextFont(),
-                      fontSize: 18,
-                      color: appConstants.getTextColor(),
-                      height: 1,
+            child: Column(
+              children: [
+                SizedBox(
+                  child: Container(
+                    height: size.height / 3,
+                    width: (size.width * 2) / 3,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(24.0),
+                      color: appConstants.getAppColor(),
                     ),
-                    textAlign: TextAlign.center,
+                    child: Center(
+                      child: Text(
+                        appConstants.getSizedBoxText(),
+                        style: TextStyle(
+                          fontFamily: appConstants.getTextFont(),
+                          fontSize: 18,
+                          color: appConstants.getTextColor(),
+                          height: 1,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
                   ),
                 ),
-              ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    primary: appConstants.getAppColor(),
+                  ),
+                  onPressed: () async {
+                    final SharedPreferences prefs = await SharedPreferences.getInstance();
+                    final String? isSuccess = prefs.getString(ApplicationConstants.PREF_BUTTON);
+                    if(isSuccess == ApplicationConstants.PREF_SUCCESS){
+                      Navigator.of(context)
+                          .push(MaterialPageRoute(builder: (context) => const SuccessScreen()));
+                    }
+                    else{
+                      Navigator.of(context)
+                          .push(MaterialPageRoute(builder: (context) => const FailScreen()));
+                    }
+                  },
+                  child: Text(
+                      appConstants.getButtonText(),
+                      style: TextStyle(
+                        fontFamily: appConstants.getTextFont(),
+                        fontSize: 18,
+                        color: appConstants.getTextColor(),
+                        height: 1,
+                      ),
+                      textAlign: TextAlign.center,
+                  ),
+                ),
+              ],
             ),
           )
         ],
